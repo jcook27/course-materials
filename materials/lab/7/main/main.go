f674f0e21bebdef2a04be8250f9c263d9de18735
jcook27 <jcook27@uwyo.edu>
2022-04-09 11:14:20 -0400

    update
    Signed-off-by: jcook27 <jcook27@uwyo.edu>

--- materials/lab/7/main/main.go
+++ materials/lab/7/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"hscan/hscan"
+	"os"
 )
 
 func main() {
@@ -18,7 +19,7 @@ func main() {
 	// Download and use bigger password file from: https://weakpass.com/wordlist/tiny  (want to push yourself try /small ; to easy? /big )
 
 	//TODO Grab the file to use from the command line instead; look at previous lab (e.g., #3 ) for examples of grabbing info from command line
-	var file = "wordlist.txt"
+	var file = os.Args[1]
 
 	hscan.GuessSingle(md5hash, file)
 	hscan.GuessSingle(sha256hash, file)
